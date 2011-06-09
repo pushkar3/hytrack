db999bfbea0c4f3b5359b97a5ef79a680a33d9b3
pushkar <pushkar@cc>
2011-06-09 18:27:21 -0400

    Fixed CMake bugs

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@
 #include "cvTracker.h"
 
 using namespace std;
+using namespace cv;
 
 int main() {
 
-	CvTracker* tracker = new CvTracker();
+	//CvTracker* tracker = new CvTracker();
 	//tracker->addObject(...);
+	Mat img;
+	img = imread("ex.jpg", CV_LOAD_IMAGE_UNCHANGED);
 
 	return 0;
 }
