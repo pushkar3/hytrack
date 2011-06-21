176dba1416064604379cd895536abd72f1d5bd82
pushkar3 <pushkarkolhe@gmail.com>
2011-06-20 21:01:36 -0400

    Added roi code

--- cvTracker.h
+++ cvTracker.h
@@ -8,6 +8,13 @@
 #ifndef CVTRACKER_H_
 #define CVTRACKER_H_
 
+#include <cv.h>
+#include <cvaux.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
 class CvROITracker {
 private:
 	int w, h;
@@ -38,29 +45,72 @@ public:
 
 class CvTracker {
 private:
-	CvSeq* tracks;		// Vector of CvROI that are being tracked
+	Mat img;
+	FeatureDetector* detector;
+	DescriptorExtractor* descriptor;
+	vector<KeyPoint> keypoints;
+
+	DescriptorMatcher* matcher;
+	vector<DMatch> matches;
+	vector<Mat> desc_vector;
+
+	Mat desc;
+	Rect roi;
+
 public:
-	CvTracker() { };
+	CvTracker() {
+		detector = new SiftFeatureDetector(SIFT::DetectorParams::GET_DEFAULT_THRESHOLD(),
+				SIFT::DetectorParams::GET_DEFAULT_EDGE_THRESHOLD(),
+				SIFT::CommonParams::AVERAGE_ANGLE);
+		// detector = new SurfFeatureDetector( 400, 3, 4);
+		// hessian threshold, octaves, octave layers
+
+		descriptor = new SurfDescriptorExtractor(3, 4, false);
+		// octaves, octave_layers, extended
+	};
+
 	~CvTracker() { };
 
-	void addObject(CvROITracker _roi) {
-		// Add _roi to tracks
+	void priorImage(Mat _img) {
+		img = _img.clone();
 	}
 
-	void deleteObject(CvROITracker _roi) {
-		// remove _roi from tracks
+	void findDescriptors() {
+		detector->detect(img, keypoints);
+		cout << "Number of keypoints: " << keypoints.size() << endl;
+
+		if(keypoints.size() > 0) {
+			descriptor->compute(img, keypoints, desc);
+			desc_vector.push_back(desc);
+		}
+
+		for(int i = 0; i < keypoints.size(); i++) {
+			circle(img, keypoints[i].pt, 2, CV_RGB(0, 255, 0));
+		}
 	}
 
-	CvROITracker* getTrackedObjects();
+	void addROI(Rect _roi) {
+		roi = _roi;
+	}
 
-	void updateTracks(CvMat* mat) {
-		/* foreach object in track
-		_roi.update(mat);
-		_roi.predict();
-		_roi.estimate();
-		_roi.smooth();
-		*/
-		// end foreach
+	Mat updateTracks(Mat _img) {
+		img = _img;
+		rectangle(img, Point(roi.x, roi.y), Point(roi.x+roi.width, roi.y+roi.height), Scalar(255, 255, 255));
+
+		int dtop = -roi.y;
+		int dbottom = -img.rows+roi.y+roi.height;
+		int dleft = -roi.x;
+		int dright = -img.cols+roi.x+roi.width;
+
+		img.adjustROI(dtop, dbottom, dleft, dright);
+		findDescriptors();
+		matcher = new BruteForceMatcher<L2<float> > ();
+
+		matcher->clear();
+		matcher->add(desc_vector);
+		matcher->match(desc, matches);
+		img.adjustROI(-dtop, -dbottom, -dleft, -dright);
+		return img;
 	}
 };
 
--- main.cpp
+++ main.cpp
@@ -3,53 +3,34 @@
 #include <highgui.h>
 #include <iostream>
 
+#include "cvTracker.h"
+
 using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv) {
 	Mat img1;
-	FeatureDetector* detector = 0;
-	DescriptorExtractor* descriptor = 0;
-	vector<KeyPoint> keypoints;
-	DescriptorMatcher* matcher = 0;
-	vector<DMatch> matches;
-	vector<Mat> desc_vector;
-
-	img1 = imread("ex.jpg", CV_LOAD_IMAGE_COLOR);
-	if (img1.empty()) {
-		cout << "Can not read image" << endl;
-		return 0;
-	}
-
-	detector = new SiftFeatureDetector(SIFT::DetectorParams::GET_DEFAULT_THRESHOLD(),
-			SIFT::DetectorParams::GET_DEFAULT_EDGE_THRESHOLD(),
-			SIFT::CommonParams::AVERAGE_ANGLE);
-	// detector = new SurfFeatureDetector( 400./*hessian_threshold*/, 3 /*octaves*/, 4/*octave_layers*/);
-
-	descriptor = new SurfDescriptorExtractor(3/*octaves*/, 4/*octave_layers*/, false/*extended*/);
-
-	detector->detect(img1, keypoints);
-	cout << "Number of keypoints: " << keypoints.size() << endl;
-
-	Mat desc;
-	if(keypoints.size() > 0) {
-		descriptor->compute(img1, keypoints, desc);
-		desc_vector.push_back(desc);
-	}
-
-	for(int i = 0; i < keypoints.size(); i++) {
-		circle(img1, keypoints[i].pt, 2, CV_RGB(0, 255, 0));
-	}
-
-	matcher = new BruteForceMatcher<L2<float> > ();
-
-	matcher->clear();
-	matcher->add(desc_vector);
-	matcher->match(desc, matches);
+	CvTracker tracker;
+	char img_file[20] = "../seqG/0001.png";
 
 	namedWindow("Win", 1);
-	imshow("Win", img1);
-	waitKey(0);
+
+	tracker.priorImage(imread(img_file, CV_LOAD_IMAGE_COLOR));
+	tracker.addROI(Rect(135, 100, 50, 50));
+
+	for(int i = 0; i < 1000; i++) {
+		sprintf(img_file, "../seqG/%04d.png", i);
+
+		img1 = imread(img_file, CV_LOAD_IMAGE_COLOR);
+		if(!img1.empty()) {
+
+			img1 = tracker.updateTracks(img1);
+
+			imshow("Win", img1);
+			waitKey(30);
+		}
+	}
 
 	return 0;
 }
+
