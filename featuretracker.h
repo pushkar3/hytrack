af122c006128b16b15c1bd729f376d4b4031d211
pushkar <pushkar@cc>
2011-06-27 14:14:25 -0400

    Cleaned featuretracker

--- featuretracker.h
+++ featuretracker.h
@@ -12,12 +12,12 @@ class FeatureTracker {
 private:
 	FeatureDetector* detector;
 	DescriptorExtractor* descriptor;
-	vector<KeyPoint> keypoints;
 	DescriptorMatcher* matcher;
-	vector<DMatch> matches;
+	vector<vector<DMatch> > matches;
 	vector<Mat> desc_vector;
 
-	Mat desc;
+	Mat roi_desc;
+	int roi_keypoints;
 	Rect trackWindow;
 
 public:
@@ -30,38 +30,52 @@ public:
 
 		descriptor = new SurfDescriptorExtractor(3, 4, false);
 		// octaves, octave_layers, extended
+
+		matcher = new BruteForceMatcher<L2<float> > ();
+		// SSD matcher
+
+		roi_keypoints = 0;
 	};
 
 	~FeatureTracker() { };
 
 	void init(Mat image, Rect selection) {
+		vector<KeyPoint> keypoints;
+
 		trackWindow = selection;
 		Mat roi(image, selection);
 		desc_vector.clear();
 		detector->detect(roi, keypoints);
 
-		if(keypoints.size() > 0) {
-			descriptor->compute(image, keypoints, desc);
-			desc_vector.push_back(desc);
+		if (keypoints.size() > 0) {
+			roi_keypoints = keypoints.size();
+			descriptor->compute(roi, keypoints, roi_desc);
+
+			for (int i = 0; i < keypoints.size(); i++) {
+				int x = selection.x + keypoints[i].pt.x;
+				int y = selection.y + keypoints[i].pt.y;
+				circle(image, Point(x, y), 2, CV_RGB(0, 255, 0));
+			}
 		}
 
-		for(int i = 0; i < keypoints.size(); i++) {
-			int x = selection.x + keypoints[i].pt.x;
-			int y = selection.y + keypoints[i].pt.y;
-			circle(image, Point(x, y), 2, CV_RGB(0, 255, 0));
-		}
 	}
 
 	Rect track(Mat image) {
+		vector<KeyPoint> keypoints;
+		Mat desc;
+
+		detector->detect(image, keypoints);
+		if(keypoints.size() > 0) {
+			descriptor->compute(image, keypoints, desc);
+			for (int i = 0; i < keypoints.size(); i++)
+				circle(image, keypoints[i].pt, 2, CV_RGB(0, 255, 0));
+
+			matcher->clear();
+			matcher->knnMatch(desc, roi_desc, matches, roi_keypoints);
+			cout << "Matches size is " << matches.size() << endl;
+		}
+
 		return trackWindow;
-	}
-
-	void match() {
-		matcher = new BruteForceMatcher<L2<float> > ();
-
-		matcher->clear();
-		matcher->add(desc_vector);
-		matcher->match(desc, matches);
 	}
 };
 
